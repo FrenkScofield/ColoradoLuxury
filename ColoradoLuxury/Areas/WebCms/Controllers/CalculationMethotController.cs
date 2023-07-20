a393d334a1c1ee804542d6aab62093464ef84f88
frenkscofield <kamranaa@code.edu.az>
2023-07-21 00:41:48 +0300

    Admin panel at side, Calculation Methots and Coupon pages added. Frontend side's ,  Booking Summary in page ,betting driver. section added.

---
+++ ColoradoLuxury/Areas/WebCms/Controllers/CalculationMethotController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ColoradoLuxury.Areas.WebCms.Controllers
+{
+    [Area("WebCms")]
+    [Route("WebCms/[controller]/[action]")]
+    public class CalculationMethotController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
