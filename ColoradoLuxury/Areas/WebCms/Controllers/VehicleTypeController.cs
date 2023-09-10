e77e333961901819731d10bb23ba6024f2b83fe5
frenkscofield <kamranaa@code.edu.az>
2023-09-11 02:35:21 +0300

    Admin panel in side  , cupon ,vehicle Type ,vehicle Add ,Edit pages ready.

--- ColoradoLuxury/Areas/WebCms/Controllers/VehicleTypeController.cs
+++ ColoradoLuxury/Areas/WebCms/Controllers/VehicleTypeController.cs
@@ -4,19 +4,19 @@ namespace ColoradoLuxury.Areas.WebCms.Controllers
 {
     [Area("WebCms")]
     [Route("WebCms/[controller]/[action]")]
-    public class ChooseVehiclePageController : Controller
+    public class VehicleTypeController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult AddVehicle()
+        public IActionResult AddVehicleType()
         {
             return View();
         }
 
-        public IActionResult EditVehicle()
+        public IActionResult EditVehicleType()
         {
             return View();
         }
