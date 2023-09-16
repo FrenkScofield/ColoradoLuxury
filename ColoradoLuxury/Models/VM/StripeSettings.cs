cdcc851263f1be9f8c274eb9d08035fa60d73f96
Emin Haciyev <eminah@code.edu.az>
2023-09-16 17:57:27 +0400

    Payment system done

---
+++ ColoradoLuxury/Models/VM/StripeSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace ColoradoLuxury.Models.VM
+{
+    public class StripeSettings
+    {
+        public string Secretkey { get; set; }
+        public string Publickey { get; set; }
+
+    }
+}
