e0b5eee08287525a98cae96f3a0d069f7d874672
frenkscofield <kamranaa@code.edu.az>
2023-09-24 18:05:40 +0400

    Admin panel in side , now all frontent  processes ready

--- ColoradoLuxury/Models/BLL/VehicleType.cs
+++ ColoradoLuxury/Models/BLL/VehicleType.cs
@@ -5,6 +5,7 @@
         public string? TypeName { get; set; }
 
         public decimal PerMile { get; set; }
+        public decimal Hourly { get; set; }
         public bool Status { get; set; }
         public ICollection<Vehicle> Vehicles { get; set; }
     }
